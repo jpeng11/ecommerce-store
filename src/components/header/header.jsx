72c360a96f81f2ce19ec17c90352dc0f9a4c4260
Percy <percy.pjc@gmail.com>
2020-10-25 22:16:06 -0400

    Added cart to the project

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,12 +3,15 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
 import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon";
+
+import Cart from "../cart/cart";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 import "./header.scss";
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
   console.log(currentUser);
   return (
     <div className="header">
@@ -31,13 +34,16 @@ const Header = ({ currentUser }) => {
             SIGN IN
           </Link>
         )}
+        <CartIcon />
       </div>
+      {hidden ? null : <Cart />}
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
